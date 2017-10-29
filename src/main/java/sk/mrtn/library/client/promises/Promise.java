2f95e0d326ce7ea874f9d75c062c0276a585eca1
Martin Liptak <martin.liptak@sowhat.sk>
2017-10-29 14:32:30 +0100

    changed promise interface

--- src/main/java/sk/mrtn/library/client/promises/Promise.java
+++ src/main/java/sk/mrtn/library/client/promises/Promise.java
@@ -14,13 +14,21 @@ public class Promise {
     @JsConstructor
     public Promise(IPromiseExecutor executor){}
 
-    public native static Promise all(Promise... promises);
+    public native static Promise all(Promise[] promises);
+    public native static Promise all(Promise promise);
+    public native static Promise all();
 
-    public native static Promise race(Promise... promises);
+    public native static Promise race(Promise[] promises);
+    public native static Promise race(Promise promise);
+    public native static Promise race();
 
-    public native static Promise reject(Promise... promises);
+    public native static Promise reject(Promise[] promises);
+    public native static Promise reject(Promise promise);
+    public native static Promise reject();
 
-    public native static Promise resolve(Promise... promises);
+    public native static Promise resolve(Promise[] promises);
+    public native static Promise resolve(Promise promise);
+    public native static Promise resolve();
 
     @JsMethod(name = "catch")
     public native Promise catch42(Object catchCallback);
